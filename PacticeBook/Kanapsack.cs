13a9c13e24ab6b1a15c07a144ddf55aae9e54675
rajku2 <raj.ku22@gmail.com>
2021-12-28 00:46:45 -0500

    Initial commit for Knapsack

---
+++ PacticeBook/Kanapsack.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PacticeBook
+{
+  public class Kanapsack
+    {
+        // This method will return the maximum profit
+        
+        public int CalculateMaxProfit(int[] profit, int[] weights, int capacity, int startIndex) 
+        {
+            return this.KnapsackRecursive(profit, weights, capacity, startIndex);
+        }
+
+        private int KnapsackRecursive(int[] profit, int[] weights, int capacity, int startIndex)
+        {
+            if (capacity == 0 || startIndex >= profit.Length) 
+            {
+                return 0;
+            }
+            int profit1 = 0;
+            if (weights[startIndex] <= capacity)
+            {
+                profit1 = profit[startIndex] + KnapsackRecursive(profit, weights, capacity - weights[startIndex], startIndex + 1);
+            }
+            int profit2 = KnapsackRecursive(profit, weights, capacity, startIndex + 1);
+            return Math.Max(profit1, profit2);            
+        }      
+
+    }
+}
---
+++ PacticeBook/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace PacticeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string s = "This : is a car123";
+            ////s.Remove()
+            //s = string.Concat(s.Where(char.IsLetterOrDigit));
+            //s = Regex.Replace(s, "[^a-zA-Z0-9]", string.Empty);
+            ////s[0].Equals(s[1],StringComparison.OrdinalIgnoreCase)
+            ////Console.WriteLine(s.ToLower());
+            ////char ape = '5';
+            ////string.Equals()
+            //HashSet<char> hschar = new HashSet<char>
+            //{'a','e','i','o','u','A','E','I','O','U'};
+
+            int[] profits= { 1, 3, 5, 7 };
+            int[] weights = { 3, 6, 8, 11 };
+            int capacity = 11;
+            Kanapsack ks = new Kanapsack();
+            int maxprofit = ks.CalculateMaxProfit(profits, weights, capacity, 0); 
+        }
+
+        //public bool CheckIfPalindrome(string input)
+        //{
+        //    char s = 'a';
+        //    s.GetType();
+        //}
+    }
+}
